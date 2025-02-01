290b312d375bd7eb5ce77d2cb358754fe553ac2a
alishka00 <balish041@gmail.com>
2025-02-01 15:32:57 +0500

    hello line

--- src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryManagementApplication {
     public static void main(String[] args) {
+        System.out.println("Inventory Management Application Started!");
         SpringApplication.run(InventoryManagementApplication.class, args);
     }
 }
